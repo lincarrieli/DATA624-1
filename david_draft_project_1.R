43a49bd6f3e7d567095e831f3803381a10c692e4
dmoste <davidmoste@gmail.com>
2021-06-23 11:44:58 -0400

    adding in come flow of consciousness

--- david_draft_project_1.R
+++ david_draft_project_1.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(mice)
 library(forecast)
+library(seasonal)
+library(urca)
 
 data <- read.csv("https://raw.githubusercontent.com/dmoste/DATA624/main/project_1_data.csv", header = TRUE)
 
@@ -11,31 +13,26 @@ s04 <- subset(data, group == "S04")
 s05 <- subset(data, group == "S05")
 s06 <- subset(data, group == "S06")
 
-ggplot(s01, aes(SeriesInd)) +
-  geom_line(aes(y = Var01, color = "Var01")) +
-  geom_line(aes(y = Var02/1000000, color = "Var02"))
+# Just working with s01v02 due to lack of missing values
+s01_v02_ts <- ts(s01[1:1622,4])
 
-frequency(s01$Var01)
+# Trying to get a visual of the data
+autoplot(s01_v02_ts)
+ggAcf(s01_v02_ts)
 
-ggAcf(s01$Var01)
-ggPacf(s01$Var01)
+# Checking what computer software thinks about differencing
+nsdiffs(s01_v02_ts)
+ndiffs(s01_v02_ts)
 
-ggplot(s02, aes(SeriesInd)) +
-  geom_line(aes(y = Var02/10000000, color = "Var02")) +
-  geom_line(aes(y = Var03, color = "Var03"))
+# Checking for stationarity. It looks like after a single difference, the data is stationary (test statistic is much smaller than 1% of critical value)
+s01_v02_ts %>% diff() %>% ggtsdisplay(main="")
 
-ggplot(s03, aes(SeriesInd)) +
-  geom_line(aes(y = Var05, color = "Var05")) +
-  geom_line(aes(y = Var07, color = "Var07"))
+s01_v02_ts %>% diff() %>% ur.kpss() %>% summary()
 
-ggplot(s04, aes(SeriesInd)) +
-  geom_line(aes(y = Var01, color = "Var01")) +
-  geom_line(aes(y = Var02/10000000, color = "Var02"))
+# Just trying an arima fit real quick to see how it looks (Since data is stationary)
+(fit <- auto.arima(s01_v02_ts, seasonal = FALSE,
+                    stepwise = FALSE, approximation = FALSE))
 
-ggplot(s05, aes(SeriesInd)) +
-  geom_line(aes(y = Var02/1000000, color = "Var02")) +
-  geom_line(aes(y = Var03, color = "Var03"))
+checkresiduals(fit)
 
-ggplot(s06, aes(SeriesInd)) +
-  geom_line(aes(y = Var05, color = "Var05")) +
-  geom_line(aes(y = Var07, color = "Var07"))
+fit %>% forecast(h = 140) %>% autoplot()
